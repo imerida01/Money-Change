4bb37263abe1e2b21aec42823eb3e3583379e57e
Kirandeep Kaur <kian16.ak@gmail.com>
2020-08-13 11:03:42 +0530

    Add files via upload

---
+++ change.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int get_change(int m)
+{
+  int n = 0 ;
+  if(m<5)
+  {
+      n=m;
+  }
+  else if( m % 10 == 0 )
+        n = m / 10;
+  else if ( m % 10 != 0 && m % 5 == 0)
+        n = ( ( m / 10 ) + 1 );
+  else if ( m % 10 != 0 && m % 5 != 0 && m % 10 > 5)
+        n = ( (m / 10) + ( ( m % 10 ) - 5 ) + 1 );
+  else
+        n = ( ( m / 10 ) + ( m % 10 ) );
+
+  return n;
+
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
